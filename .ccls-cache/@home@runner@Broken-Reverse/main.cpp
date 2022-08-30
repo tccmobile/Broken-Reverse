73949f079899ccf1f25a0f7cc4205ac332398d8f
William Smith <William.smith@tulsacc.edu>
2022-08-30 13:38:35 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Broken-Reverse/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   const int NUM_ELEMENTS = 8;        // Number of elements
+   vector<int> revVctr(NUM_ELEMENTS); // User values
+   unsigned int i;                    // Loop index
+       
+   cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << "Value: ";
+      cin >> revVctr.at(i);
+   }
+   
+   // Reverse
+   for (i = 0; i < revVctr.size(); ++i) {
+      revVctr.at(i) = revVctr.at(revVctr.size() - i); // Swap
+   }
+   
+   // Print values
+   cout << endl << "New values: ";
+   for (i = 0; i < revVctr.size(); ++i) {
+      cout << " " << revVctr.at(i);
+   }
+   cout << endl;
+   
+   return 0;
+}
